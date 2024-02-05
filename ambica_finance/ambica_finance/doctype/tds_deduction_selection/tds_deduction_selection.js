24f105a62fa907dbcdf4b4e73a3e1e9ad2653ab8
Srushti238 <srushti@sanskartechnolab.com>
2024-02-05 17:26:25 +0530

    tds deduction selection

--- ambica_finance/ambica_finance/doctype/tds_deduction_selection/tds_deduction_selection.js
+++ ambica_finance/ambica_finance/doctype/tds_deduction_selection/tds_deduction_selection.js
@@ -1,8 +1,38 @@
 // Copyright (c) 2023, riddhi and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("TDS deduction selection", {
-// 	refresh(frm) {
+frappe.ui.form.on("TDS deduction selection", {
+	refresh(frm) {
 
-// 	},
-// });
+	},
+
+    category:function(frm){
+        frappe.call({
+            method: "ambica_finance.ambica_finance.doctype.tds_deduction_selection.tds_deduction_selection.Tdsdeductionselection",
+            args:{
+                'category':frm.doc.category
+            },
+            callback:function(r){
+                console.log(r.message)
+                frm.clear_table('tds_deduction');
+                
+                // Create second row in the child table using 'twcategory' data
+                let row_twcategory = frappe.model.add_child(frm.doc, 'TDS Deduction', 'tds_deduction');
+                row_twcategory.sub_category = r.message.twcategory[0].name;
+                row_twcategory.base_amount = r.message.purchase_invoices_for_category[0].base_amount;
+                row_twcategory.tds_rate = r.message.twcategory[0].tax_withholding_rate;
+                row_twcategory.tds_amount = r.message.purchase_invoices_for_category[0].tds_amount;
+                // Create first row in the child table using 'twc' data
+                let row_twc = frappe.model.add_child(frm.doc, 'TDS Deduction', 'tds_deduction');
+                row_twc.sub_category = r.message.twc[0].name;
+                row_twc.base_amount = r.message.purchase_invoices_fortwc[0].base_amount;
+                row_twc.tds_rate = r.message.twc[0].tax_withholding_rate;
+                row_twc.tds_amount = r.message.purchase_invoices_fortwc[0].tds_amount;
+
+
+                // Refresh the child table
+                frm.refresh_field('tds_deduction');
+            }
+        });
+    }
+});
--- ambica_finance/ambica_finance/doctype/tds_deduction_selection/tds_deduction_selection.py
+++ ambica_finance/ambica_finance/doctype/tds_deduction_selection/tds_deduction_selection.py
@@ -1,9 +1,51 @@
 # Copyright (c) 2023, riddhi and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class TDSdeductionselection(Document):
 	pass
+
+
+@frappe.whitelist()
+def Tdsdeductionselection(category):
+    sql = """
+        SELECT twc.name, twr.tax_withholding_rate, twr.from_date, twr.to_date 
+        FROM `tabTax Withholding Category` AS twc 
+        INNER JOIN `tabTax Withholding Rate` AS twr ON twr.parent = twc.name
+        WHERE twc.name LIKE %s
+    """
+
+    # Extracting the first three elements and creating the pattern
+    category_pattern = f"{' - '.join(category.split(' - ')[:3])}%"
+    
+    twcategory = frappe.db.sql(sql, category, as_dict=True)
+    twc = frappe.db.sql(sql, f'{category_pattern} - Other', as_dict=True)
+
+    from_date = twcategory[0].get('from_date')
+    to_date = twcategory[0].get('to_date')
+    
+    from_date_twc = twc[0].get('from_date')
+    to_date_twc = twc[0].get('to_date')
+
+    sql_purchase_invoices_for_category = """
+        SELECT SUM(pi.total) as base_amount, SUM(ptc.tax_amount) as tds_amount 
+        FROM `tabPurchase Invoice` AS pi 
+        INNER JOIN `tabPurchase Taxes and Charges` AS ptc ON ptc.parent = pi.name 
+        WHERE pi.tax_withholding_category = %s AND pi.posting_date BETWEEN %s AND %s
+    """
+
+    purchase_invoices_for_category = frappe.db.sql(sql_purchase_invoices_for_category, (category, from_date, to_date), as_dict=True)
+    
+    purchase_invoices_fortwc = frappe.db.sql(sql_purchase_invoices_for_category, (category_pattern, from_date_twc, to_date_twc), as_dict=True)
+
+    return {
+        'twcategory': twcategory,
+        'twc': twc,
+        'purchase_invoices_for_category': purchase_invoices_for_category,
+        'purchase_invoices_fortwc': purchase_invoices_fortwc
+    }
+
+
